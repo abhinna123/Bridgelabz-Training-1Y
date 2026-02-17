0c660016ab9668683cd4fcb9a60833a05b8ae709
abhinna123 <singhabhinna2007@gmail.com>
2026-02-17 08:41:14 +0530

    adding stringapi programs

--- basic-programs/01-Practice-Problems/WelcomeBridgelabz.java
+++
@@ -1,8 +0,0 @@
-import java.util.Scanner;
-
-public class WelcomeBridgelabz {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Welcome to Bridgelabz!");
-    }
-}
